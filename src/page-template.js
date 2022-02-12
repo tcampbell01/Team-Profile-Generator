93bb7f42cea07f39d086b5a8d6376673db5c03c6
TC <teresa.j.campbell@gmail.com>
2022-02-12 13:58:44 -0500

    added github link

--- src/page-template.js
+++ src/page-template.js
@@ -32,7 +32,7 @@ const generateTeam = team => {
         <ul class = "list-group">
             <li class="id">ID: ${engineer.getId()}</li>
             <li class="email">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="gitHub">GitHub: ${engineer.getGitHub()}</li>
+            <li class="gitHub">GitHub: <a href="github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</a></li>
         </ul>
     </div>
 </div>
